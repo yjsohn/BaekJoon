b48708ed4bdb428c39ade7a87f6e2cfd7bf661cd
username <user@email.mail>
2021-03-09 18:30:48 +0900

    [11779] 최소비용 구하기2

--- 11779.cpp
+++ 11779.cpp
@@ -10,7 +10,7 @@ int N, M;
 int d[1001];
 vector<pair<int, int>> graph[1001];
 priority_queue<pair<int, int>> pq;
-int before[1001];	//최적의 경로가 갱신된다면 부모 노드를 저장해주기
+int parent[1001];	//최적의 경로가 갱신된다면 부모 노드를 저장해주기
 
 void dijkstra(int start) {
 	d[start] = 0;
@@ -27,18 +27,18 @@ void dijkstra(int start) {
 			if (cost < d[node]) {
 				d[node] = cost;
 				pq.push({ -cost, node });
-				before[node] = now;	//경로 갱신 -> 부모 노드 저장
+				parent[node] = now;	//경로 갱신 -> 부모 노드 저장
 			}
 		}
 	}
 }
 
-void Track(int start, int end) {    //거쳐가는 도시 수, 경로 출력
+void Track(int start, int end) {	//거쳐가는 도시 수, 내용
 	int node = end;
 	stack<int> s;	//역추적이기 때문에 stack에 경로 저장 후 top부터 출력
 	while (node != 0) {
 		s.push(node);
-		node = before[node];
+		node = parent[node];
 	}
 	cout << s.size() << "\n";	//!거쳐가는 도시 개수 반드시 출력해줄 것! - 처음에 이거 없어서 WA
 	while (!s.empty()) {
